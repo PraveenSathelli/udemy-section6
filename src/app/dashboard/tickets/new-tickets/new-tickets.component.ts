6b5dfaee599717d6462d3f6ca8a3a72e861bb676
Praveen Sathelli <praveensathelli11@gmail.com>
2026-09-05 19:27:35 +0530

    HTMLInputElement and #template filed values read

--- src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css'
 })
 export class NewTicketsComponent {
 
+  onSubmit(titleName: HTMLInputElement) {
+    console.dir(titleName.value)
+    console.log("submit called")
+  }
 }
