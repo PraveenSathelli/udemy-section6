6db7c41633a75f1817c83bb16dff0f3182bf2364
Praveen Sathelli <praveensathelli11@gmail.com>
2026-09-05 21:29:54 +0530

    afterview init rather than init

--- src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, viewChild, ViewChild } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 import { FormsModule } from '@angular/forms';
@@ -10,15 +10,27 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css'
 })
-export class NewTicketsComponent {
-  // @ViewChild('form')
-  // form?:ElementRef<HTMLFormElement>;
+export class NewTicketsComponent implements OnInit, AfterViewInit {
+
+  
+  @ViewChild('form')
+  form?:ElementRef<HTMLFormElement>;
   //signal way
-  form = viewChild.required<ElementRef<HTMLFormElement>>('form');
+  // form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(title: string, ticketText: string) {
     console.log(title, ticketText);
-    this.form().nativeElement.reset();
+    this.form?.nativeElement.reset();
     console.log("submit called")
+  }
+  ngOnInit() {
+    console.log('INIT');
+    console.log(this.form);
+    console.log("-----end init----")
+  }
+  ngAfterViewInit() {
+    console.log('view INIT');
+    console.log(this.form);
+    console.log("-----end afterview init----")
   }
 }
